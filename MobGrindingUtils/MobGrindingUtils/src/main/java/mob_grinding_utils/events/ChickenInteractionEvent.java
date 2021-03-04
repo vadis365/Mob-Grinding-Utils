12af52bb039cc14b1454d68de4db5e4a68d5cc1e
vadis365 <curve_linear@hotmail.com>
2021-03-04 20:31:21 +0000

    Chicken interaction event
    removing errors and commenting out things to fix :P

--- MobGrindingUtils/MobGrindingUtils/src/main/java/mob_grinding_utils/events/ChickenInteractionEvent.java
+++ MobGrindingUtils/MobGrindingUtils/src/main/java/mob_grinding_utils/events/ChickenInteractionEvent.java
@@ -1,42 +1,46 @@
 package mob_grinding_utils.events;
 
-import mob_grinding_utils.MobGrindingUtils;
 import mob_grinding_utils.ModItems;
-import mob_grinding_utils.network.MessageChickenSync;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.passive.EntityChicken;
-import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.passive.ChickenEntity;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.EntityInteract;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class ChickenInteractionEvent {
 
 	@SubscribeEvent
 	public void clickOnChicken(EntityInteract event) {
-		if (event.getTarget() instanceof EntityLivingBase) {
-			EntityLivingBase entity = (EntityLivingBase) event.getTarget();
+		if (event.getTarget() instanceof LivingEntity) {
+			LivingEntity entity = (LivingEntity) event.getTarget();
 
-			if (entity instanceof EntityChicken && !entity.isChild()) {
+			if (entity instanceof ChickenEntity && !entity.isChild()) {
 					World world = entity.getEntityWorld();
 					if (!event.getItemStack().isEmpty() && event.getItemStack().getItem() == ModItems.GM_CHICKEN_FEED) {
 						if (!world.isRemote) {
-							NBTTagCompound nbt = new NBTTagCompound();
-							nbt = entity.getEntityData();
-							if (!nbt.hasKey("shouldExplode")) {
-								entity.setEntityInvulnerable(true);
-								nbt.setBoolean("shouldExplode", true);
-								nbt.setInteger("countDown", 0);
-								if (event.getItemStack().hasTagCompound() && event.getItemStack().getTagCompound().hasKey("mguMobName"))
-									nbt.setString("mguMobName", event.getItemStack().getTagCompound().getString("mguMobName"));
-								if (event.getItemStack().hasTagCompound() && event.getItemStack().getTagCompound().hasKey("chickenType"))
-									nbt.setString("chickenType", event.getItemStack().getTagCompound().getString("chickenType"));
+							CompoundNBT nbt = new CompoundNBT();
+							nbt = entity.getPersistentData();
+							if (!nbt.contains("shouldExplode")) {
+								entity.setInvulnerable(true);
+								nbt.putBoolean("shouldExplode", true);
+								nbt.putInt("countDown", 0);
+								if (event.getItemStack().hasTag() && event.getItemStack().getTag().contains("mguMobName"))
+									nbt.putString("mguMobName", event.getItemStack().getTag().getString("mguMobName"));
+								
+							/*	//TODO Remove chickens mod compat stuff
+								if (event.getItemStack().hasTag() && event.getItemStack().getTag().contains("chickenType"))
+									nbt.setString("chickenType", event.getItemStack().getTag().getString("chickenType"));
+							*/	
+								
 								//MobGrindingUtils.NETWORK_WRAPPER.sendToAll(new MessageChickenSync(entity, nbt)); //todo
 							}
-							entity.motionY += (0.06D * (double) (10) - entity.motionY) * 0.2D;
-							((EntityChicken) entity).setNoGravity(true);
+							Vector3d vec3d = entity.getMotion();
+							entity.setMotion(vec3d.x, (double) 0.06D, vec3d.z); // will need adjusting to right amount later
+							((ChickenEntity) entity).setNoGravity(true);
 
-							if (!event.getEntityPlayer().capabilities.isCreativeMode)
+							if (!event.getPlayer().abilities.isCreativeMode)
 								event.getItemStack().shrink(1);
 						}
 					}
