34498ae7bdab973aa329c6360b7f87650a63a7fb
vadis365 <curve_linear@hotmail.com>
2021-03-04 23:29:14 +0000

    Entity conveyer
    fixy comes later - can probably redo the motion stuff now

--- MobGrindingUtils/MobGrindingUtils/src/main/java/mob_grinding_utils/blocks/BlockEntityConveyor.java
+++ MobGrindingUtils/MobGrindingUtils/src/main/java/mob_grinding_utils/blocks/BlockEntityConveyor.java
@@ -1,103 +1,90 @@
 package mob_grinding_utils.blocks;
 
-import javafx.geometry.Side;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockHorizontal;
-import net.minecraft.block.properties.IProperty;
-import net.minecraft.block.properties.PropertyDirection;
-import net.minecraft.block.state.BlockStateContainer;
-import net.minecraft.block.state.IBlockState;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalBlock;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.boss.EntityDragon;
-import net.minecraft.entity.boss.EntityWither;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockEntityConveyor extends Block {
-	public static final PropertyDirection FACING = BlockHorizontal.FACING;
-	public static final AxisAlignedBB CONVEYOR_AABB = new AxisAlignedBB(0D, 0D, 0D, 1D, 0.875D, 1D);
+	public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+	public static final VoxelShape CONVEYOR_AABB = Block.makeCuboidShape(0D, 0D, 0D, 1D, 0.875D, 1D);
 
 	public BlockEntityConveyor(Block.Properties properties) {
 		super(properties);
 	}
 
 	@Override
-	public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
+	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
 		return CONVEYOR_AABB;
 	}
-
+	
 	@Override
-	@SideOnly(Side.CLIENT)
-	public AxisAlignedBB getSelectedBoundingBox(IBlockState state, World worldIn, BlockPos pos) {
-		return FULL_BLOCK_AABB;
+	public VoxelShape getRaytraceShape(BlockState state, IBlockReader worldIn, BlockPos pos) {
+		return VoxelShapes.fullCube();
 	}
-
+	
+/*
 	@Override
 	public boolean canEntityDestroy(IBlockState state, IBlockAccess world, BlockPos pos, Entity entity) {
       return !(entity instanceof EntityWither) && !(entity instanceof EntityDragon);
     }
+    
+//TODO Add to block properties 
 
 	@Override
-    public boolean canEntitySpawn(IBlockState state, Entity entityIn) {
+    public boolean canEntitySpawn(BlockState state, Entity entityIn) {
         return true;
     }
+*/
 
 	@Override
-	public IBlockState getStateFromMeta(int meta) {
-		EnumFacing facing = EnumFacing.byIndex(meta);
-		if (facing.getAxis() == EnumFacing.Axis.Y)
-			facing = EnumFacing.NORTH;
-		return getDefaultState().withProperty(FACING, facing);
+	public BlockState getStateForPlacement(BlockItemUseContext context) {
+		Direction direction = context.getFace().getOpposite();
+		return this.getDefaultState().with(FACING, direction);
+	}
+	
+	@Override
+	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+		builder.add(FACING);
 	}
 
 	@Override
-	public int getMetaFromState(IBlockState state) {
-		int meta = 0;
-		meta = meta | ((EnumFacing) state.getValue(FACING)).getIndex();
-		return meta;
-	}
-
-	@Override
-	 public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
-		return getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
-	}
-
-	@Override
-	protected BlockStateContainer createBlockState() {
-		return new BlockStateContainer(this, new IProperty[] { FACING });
-	}
-
-	@Override
-	public void onEntityCollision(World world, BlockPos pos, IBlockState state, Entity entity) {
+	public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
 		if (entity.isSneaking())
 			return;
 
-		if (entity instanceof EntityLiving)
-			((EntityLiving) entity).enablePersistence();
-		else if (entity instanceof EntityItem)
-			((EntityItem) entity).setNoDespawn();
+		if (entity instanceof MobEntity)
+			((MobEntity) entity).enablePersistence();
+		else if (entity instanceof ItemEntity)
+			((ItemEntity) entity).setNoDespawn();
 
 		double speed = speed();
-		int meta = state.getValue(FACING).getIndex() - 2;
+		int meta = state.get(FACING).getIndex() - 2;
 		int[] factorX = { 0, 0, -1, 1 };
 		int[] factorZ = { -1, 1, 0, 0 };
 
-		if (entity.posY > pos.getY() + 0.5D) {
-			if (factorX[meta] == 0 && Math.abs(pos.getX() + 0.5D - entity.posX) < 0.5D && Math.abs(pos.getX() + 0.5D - entity.posX) > 0.1D)
-				entity.motionX += Math.signum(pos.getX() + 0.5D - entity.posX) * Math.min(speed, Math.abs(pos.getX() + 0.5D - entity.posX)) / 1.2D;
+		if (entity.getPosY() > pos.getY() + 0.5D) {
+			Vector3d vec3d = entity.getMotion();
+			if (factorX[meta] == 0 && Math.abs(pos.getX() + 0.5D - entity.getPosX()) < 0.5D && Math.abs(pos.getX() + 0.5D - entity.getPosX()) > 0.1D)
+				entity.setMotion(vec3d.x + Math.signum(pos.getX() + 0.5D - entity.getPosX()) * Math.min(speed, Math.abs(pos.getX() + 0.5D - entity.getPosX())) / 1.2D, vec3d.y, vec3d.z);
+			if (factorZ[meta] == 0 && Math.abs(pos.getZ() + 0.5D - entity.getPosZ()) < 0.5D && Math.abs(pos.getZ() + 0.5D - entity.getPosZ()) > 0.1D)
+				entity.setMotion(vec3d.x, vec3d.y, vec3d.z + Math.signum(pos.getZ() + 0.5D - entity.getPosZ()) * Math.min(speed, Math.abs(pos.getZ() + 0.5D - entity.getPosZ())) / 1.2D);
 
-			if (factorZ[meta] == 0 && Math.abs(pos.getZ() + 0.5D - entity.posZ) < 0.5D && Math.abs(pos.getZ() + 0.5D - entity.posZ) > 0.1D)
-				entity.motionZ += Math.signum(pos.getZ() + 0.5D - entity.posZ) * Math.min(speed, Math.abs(pos.getZ() + 0.5D - entity.posZ)) / 1.2D;
+			entity.setMotion(vec3d.x + factorX[meta] * speed, vec3d.y, vec3d.z + factorZ[meta] * speed);
 
-			entity.motionX += factorX[meta] * speed;
-			entity.motionZ += factorZ[meta] * speed;
 		}
 	}
 
