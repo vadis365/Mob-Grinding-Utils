8595ab9c7547e28d697516de447f6be72d0e93e8
Flanks255 <32142731+Flanks255@users.noreply.github.com>
2021-03-07 18:50:50 -0600

    Cleaned up the tag datagen, bumped forge a bit.

--- MobGrindingUtils/MobGrindingUtils/src/main/java/mob_grinding_utils/datagen/MGUBlockTags.java
+++ MobGrindingUtils/MobGrindingUtils/src/main/java/mob_grinding_utils/datagen/MGUBlockTags.java
@@ -1,5 +1,6 @@
 package mob_grinding_utils.datagen;
 
+import mob_grinding_utils.MobGrindingUtils;
 import mob_grinding_utils.ModBlocks;
 import mob_grinding_utils.Reference;
 import net.minecraft.block.Block;
@@ -17,32 +18,10 @@ public class MGUBlockTags extends BlockTagsProvider {
     protected void registerTags() {
         Builder<Block> witherTag = this.getOrCreateBuilder(BlockTags.WITHER_IMMUNE);
         Builder<Block> dragonTag = this.getOrCreateBuilder(BlockTags.DRAGON_IMMUNE);
-        witherTag.add(ModBlocks.ABSORPTION_HOPPER);
-        witherTag.add(ModBlocks.DARK_OAK_STONE);
-        witherTag.add(ModBlocks.DRAGON_MUFFLER);
-        witherTag.add(ModBlocks.ENDER_INHIBITOR_OFF);
-        witherTag.add(ModBlocks.ENDER_INHIBITOR_ON);
-        witherTag.add(ModBlocks.ENTITY_CONVEYOR);
-        witherTag.add(ModBlocks.FAN);
-        witherTag.add(ModBlocks.SAW);
-        witherTag.add(ModBlocks.SPIKES);
-        witherTag.add(ModBlocks.TANK);
-        witherTag.add(ModBlocks.TANK_SINK);
-        witherTag.add(ModBlocks.WITHER_MUFFLER);
-        witherTag.add(ModBlocks.XP_TAP);
 
-        dragonTag.add(ModBlocks.ABSORPTION_HOPPER);
-        dragonTag.add(ModBlocks.DARK_OAK_STONE);
-        dragonTag.add(ModBlocks.DRAGON_MUFFLER);
-        dragonTag.add(ModBlocks.ENDER_INHIBITOR_OFF);
-        dragonTag.add(ModBlocks.ENDER_INHIBITOR_ON);
-        dragonTag.add(ModBlocks.ENTITY_CONVEYOR);
-        dragonTag.add(ModBlocks.FAN);
-        dragonTag.add(ModBlocks.SAW);
-        dragonTag.add(ModBlocks.SPIKES);
-        dragonTag.add(ModBlocks.TANK);
-        dragonTag.add(ModBlocks.TANK_SINK);
-        dragonTag.add(ModBlocks.WITHER_MUFFLER);
-        dragonTag.add(ModBlocks.XP_TAP);
+        for (Block block : ModBlocks.BLOCKS) {
+            witherTag.add(block);
+            dragonTag.add(block);
+        }
     }
 }
