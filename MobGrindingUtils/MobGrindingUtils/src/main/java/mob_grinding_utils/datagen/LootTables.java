2042828f3bcebfac25e29984ccd1dc9ab59457bd
Flanks255 <32142731+Flanks255@users.noreply.github.com>
2021-03-21 18:45:06 -0500

    Tinted glass Tags/Loot Tables

--- MobGrindingUtils/MobGrindingUtils/src/main/java/mob_grinding_utils/datagen/LootTables.java
+++ MobGrindingUtils/MobGrindingUtils/src/main/java/mob_grinding_utils/datagen/LootTables.java
@@ -51,6 +51,7 @@ public class LootTables extends LootTableProvider {
             registerDropSelfLootTable(ModBlocks.XP_TAP);
             registerDropSelfLootTable(ModBlocks.ENDER_INHIBITOR_ON);
             registerDropping(ModBlocks.ENDER_INHIBITOR_OFF, ModBlocks.ENDER_INHIBITOR_ON_ITEM);
+            registerDropSelfLootTable(ModBlocks.TINTED_GLASS);
         }
 
         @Nonnull
