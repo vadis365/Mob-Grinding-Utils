49f3e9f43a7e10396a0a7d55d65baabec0132609
Flanks255 <32142731+Flanks255@users.noreply.github.com>
2022-01-28 17:27:15 -0600

    Fixed the Fakeplayer always being at 0 0 0, causing chunks to load.

--- MobGrindingUtils/MobGrindingUtils/src/main/java/mob_grinding_utils/fakeplayer/MGUFakePlayer.java
+++ MobGrindingUtils/MobGrindingUtils/src/main/java/mob_grinding_utils/fakeplayer/MGUFakePlayer.java
@@ -5,9 +5,11 @@ import java.lang.ref.WeakReference;
 import com.mojang.authlib.GameProfile;
 
 import mob_grinding_utils.Reference;
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.util.FakePlayer;
 
 public class MGUFakePlayer extends FakePlayer {
@@ -16,6 +18,8 @@ public class MGUFakePlayer extends FakePlayer {
     }
 
     private static MGUFakePlayer INSTANCE;
+    protected Vec3 pos = new Vec3(0, 0, 0);
+    protected BlockPos.MutableBlockPos blockPos = new BlockPos.MutableBlockPos(0, 0, 0);
 
     public static WeakReference<MGUFakePlayer> get(ServerLevel world) {
         if (INSTANCE == null) {
@@ -32,7 +36,15 @@ public class MGUFakePlayer extends FakePlayer {
         }
         INSTANCE.level = world;
         INSTANCE.setPos(x,y,z);
+        INSTANCE.setPosition(x,y,z);
         return new WeakReference<>(INSTANCE);
+    }
+
+    public void setPosition(double x, double y, double z) {
+        if (pos.x != x || pos.y != y || pos.z != z) {
+            pos = new Vec3(x, y, z);
+            blockPos.set(Math.floor(x), Math.floor(y), Math.floor(z));
+        }
     }
 
     @Override
@@ -43,5 +55,15 @@ public class MGUFakePlayer extends FakePlayer {
     public static void unload(LevelAccessor world) {
         if (INSTANCE != null && INSTANCE.level == world)
             INSTANCE = null;
+    }
+
+    @Override
+    public Vec3 position() {
+        return this.pos;
+    }
+
+    @Override
+    public BlockPos blockPosition() {
+        return this.blockPos;
     }
 }
