aff353e4bcd9f39a8729cb0cb09fc3e4359ceaf7
lollopanta <lollopanta09@gmail.com>
2026-08-21 16:44:48 +0200

    Fix zombie fake-player damage mixin

--- MobGrindingUtils/MobGrindingUtils/src/main/java/mob_grinding_utils/mixin/ZombieMixin.java
+++ MobGrindingUtils/MobGrindingUtils/src/main/java/mob_grinding_utils/mixin/ZombieMixin.java
@@ -1,6 +1,7 @@
 package mob_grinding_utils.mixin;
 
 import mob_grinding_utils.util.FakePlayerHandler;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.monster.zombie.Zombie;
 import net.neoforged.neoforge.common.util.FakePlayer;
@@ -11,8 +12,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Zombie.class)
 public class ZombieMixin {
-    @Inject(method = "hurt", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/damagesource/DamageSource;getEntity()Lnet/minecraft/world/entity/Entity;", ordinal = 0), cancellable = true)
-    public void hurtMixin(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "hurtServer", at = @At("HEAD"), cancellable = true)
+    public void hurtMixin(ServerLevel level, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         if (source.getEntity() instanceof FakePlayer fakePlayer && FakePlayerHandler.isMGUFakePlayer(fakePlayer)) {
                 cir.setReturnValue(false);
         }
